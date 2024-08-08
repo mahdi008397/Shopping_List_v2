497f5ba68dee44679253457f461a07a30d109979
Mahdi008397 <Mahdi008397@gmail.com>
2024-08-08 21:07:47 +0330

    First Commit 📺

---
+++ README.md
@@ -0,0 +1,3 @@
+# shopping_list_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/shopping_list_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopping_list_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
